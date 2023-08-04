5016260d12d3f7dbeac2bfa4822e277ee24ad65b
Niko Korvenlaita <n.p.ahonen@gmail.com>
2023-08-04 18:45:02 +0300

    add snowflake test setup

--- integration_tests/tests/test_normalize_email.sql
+++ integration_tests/tests/test_normalize_email.sql
@@ -1,23 +1,3 @@
-{% macro email_test_data() %}
-{{ return(adapter.dispatch('email_test_data')()) }}
-{% endmacro %}
-{% macro bigquery__email_test_data() %}
-with d1 as (
-  select * from unnest([
-    STRUCT('a@a.a' as input, 'a@a.a' as expected),
-    ('FOO@bar.IO', 'foo@bar.io'),
-    (null, null),
-    ('not an email', null),
-    ('almost@email', null)
-  ])
-), d2 as (
-  select * from unnest([
-    STRUCT(1 as input, cast(null as string) as expected)
-  ])
-),
-{% endmacro %}
-
-{% macro redshift__email_test_data() %}
 with d1 as (
   select 'a@a.a' as input, 'a@a.a' as expected
   union all
@@ -29,11 +9,8 @@ with d1 as (
   union all
   select 'almost@email', null
 ), d2 as (
-  select 1 as input, cast(null as text) as expected
+  select 1 as input, cast(null as {{ type_string() }}) as expected
 ),
-{% endmacro %}
-
-{{ email_test_data() }}
 
 tst as (
   select
@@ -47,4 +24,4 @@ tst as (
 
 select 1
 from tst
-where success is false
+where not success
--- run_tests.sh
+++ run_tests.sh
@@ -3,3 +3,4 @@
 dbt clean --project-dir ./integration_tests
 dbt test --project-dir ./integration_tests --profile reconfigured_test_redshift
 dbt test --project-dir ./integration_tests --profile reconfigured_test_bigquery
+dbt test --project-dir ./integration_tests --profile reconfigured_test_snowflake
