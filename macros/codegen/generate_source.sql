ebe14b2e7494764e2ceacdb61ec2b9f255879d8d
Nipsuli <n.p.ahonen@gmail.com>
2023-11-03 09:17:19 +0200

    add generate sources
    This is modified version of the dbt-codegen

---
+++ macros/codegen/generate_source.sql
@@ -0,0 +1,74 @@
+/* Patched version of dbt-codegen generate_source
+ * TODO: open pr to dbt-codegen
+ */
+-------
+{% macro generate_source(schema_name, database_name=target.database, generate_columns=False, include_descriptions=False, include_data_types=True, table_pattern='%', exclude='', name=schema_name, table_names=None, include_database=False, include_schema=False) %}
+
+{% set sources_yaml=[''] %}
+{% do sources_yaml.append('version: 2') %}
+{% do sources_yaml.append('') %}
+{% do sources_yaml.append('sources:') %}
+{% do sources_yaml.append('  - name: ' ~ name | lower) %}
+
+{% if include_descriptions %}
+    {% do sources_yaml.append('    description: ""' ) %}
+{% endif %}
+
+{% if database_name != target.database or include_database %}
+{% do sources_yaml.append('    database: ' ~ database_name | lower) %}
+{% endif %}
+
+{% if schema_name != name or include_schema %}
+{% do sources_yaml.append('    schema: ' ~ schema_name | lower) %}
+{% endif %}
+
+{% do sources_yaml.append('    tables:') %}
+
+{% if table_names is none %}
+{% set tables=codegen.get_tables_in_schema(schema_name, database_name, table_pattern, exclude) %}
+{% else %}
+{% set tables = table_names %}
+{% endif %}
+
+{% for table in tables %}
+    {% do sources_yaml.append('      - name: ' ~ table | lower ) %}
+    {% do sources_yaml.append('        identifier: ' ~ table ) %}
+    {% if include_descriptions %}
+        {% do sources_yaml.append('        description: ""' ) %}
+    {% endif %}
+    {% if generate_columns %}
+    {% do sources_yaml.append('        columns:') %}
+
+        {% set table_relation=api.Relation.create(
+            database=database_name,
+            schema=schema_name,
+            identifier=table,
+            quote_policy={ "identifier": true }
+        ) %}
+
+        {% set columns=adapter.get_columns_in_relation(table_relation) %}
+
+        {% for column in columns %}
+            {% do sources_yaml.append('          - name: ' ~ column.name ) %}
+            {% if include_data_types %}
+                {% do sources_yaml.append('            data_type: ' ~ codegen.data_type_format_source(column)) %}
+            {% endif %}
+            {% if include_descriptions %}
+                {% do sources_yaml.append('            description: ""' ) %}
+            {% endif %}
+        {% endfor %}
+            {% do sources_yaml.append('') %}
+
+    {% endif %}
+
+{% endfor %}
+
+{% if execute %}
+
+    {% set joined = sources_yaml | join ('\n') %}
+    {{ log(joined, info=True) }}
+    {% do return(joined) %}
+
+{% endif %}
+
+{% endmacro %}
